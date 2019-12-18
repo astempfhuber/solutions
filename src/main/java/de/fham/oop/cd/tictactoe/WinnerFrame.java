9eee6e196082ec9470bbc9f7ce64dd9306bd0939
c-dietert <christoph.dietert@gmx.net>
2019-12-18 10:08:57 +0100

    implmented solution for tic tac toe

---
+++ src/main/java/de/fham/oop/cd/tictactoe/WinnerFrame.java
@@ -0,0 +1,20 @@
+package de.fham.oop.cd.tictactoe;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class WinnerFrame extends JFrame {
+
+    public WinnerFrame() throws HeadlessException {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(300, 200);
+    }
+
+    public void winnerIs(String winner) {
+        if ("O".equals(winner)) {
+            this.setTitle("The winner is player 1!");
+        } else {
+            this.setTitle("The winner is player 2!");
+        }
+    }
+}
